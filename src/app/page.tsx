4c1324b342e96856c1ad994ded1c845c195528f1
Akshay Gangurde <agangurde771@gmail.com>
2025-04-26 11:38:08 +0530

    fixed useSerachParams

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import ListingsClient from "@/components/ListingsClient";
+import { Suspense } from "react";
 
 export default async function ListingsPage() {
   return (
     <div>
       <Navbar />
-      <ListingsClient />
+      <Suspense fallback={<div>Loading listings...</div>}>
+        <ListingsClient />
+      </Suspense>
       <Footer />
     </div>
   );
