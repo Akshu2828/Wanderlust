292cd0bbdb7bf8a215b1d1407b2cc038cd750181
Akshay Gangurde <agangurde771@gmail.com>
2025-04-27 11:21:29 +0530

    fixed createlisting 7

--- src/app/api/createListing/route.ts
+++ src/app/api/createListing/route.ts
@@ -83,6 +83,11 @@ export async function POST(req: NextRequest) {
     const arrayBuffer = await file.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
     console.log("Connecting to Cloudinary...");
+    console.log("Cloudinary config check:", {
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
 
     const uploadResult = await uploadImage(buffer);
     console.log("Uploaded image:", uploadResult);
